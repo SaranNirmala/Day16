f44857b4b4263daf39dfd50a5b744a15c8244b07
SaranNirmala <126362188+SaranNirmala@users.noreply.github.com>
2023-05-07 02:03:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+let outputdata=document.getElementById("display");
+
+outputdata.style.textAlign="center";
+
+    setTimeout(()=>{
+        console.log("10");
+        outputdata.style.color="blue";
+        outputdata.innerHTML=10;
+        setTimeout(()=>{
+            console.log("9");
+            outputdata.style.color="red";
+            outputdata.innerHTML=9;
+            setTimeout(()=>{
+                console.log("8");
+                outputdata.style.color="purple";
+                outputdata.innerHTML=8;
+                setTimeout(()=>{
+                    console.log("7");
+                    outputdata.style.color="green";
+                    outputdata.innerHTML=7;
+                    setTimeout(()=>{
+                        console.log("6");
+                        outputdata.style.color="blue";
+                        outputdata.innerHTML=6;
+                        setTimeout(()=>{
+                            console.log("5");
+                            outputdata.style.color="red";
+                            outputdata.innerHTML=5;
+                            setTimeout(()=>{
+                                console.log("4");
+                                outputdata.style.color="purple";
+                                outputdata.innerHTML=4;
+                                setTimeout(()=>{
+                                    console.log("3");
+                                    outputdata.style.color="Green";
+                                    outputdata.innerHTML=3;
+                                    setTimeout(()=>{
+                                        console.log("2");
+                                        outputdata.style.color="blue";
+                                        outputdata.innerHTML=2;
+                                        setTimeout(()=>{
+                                            console.log("1");
+                                            outputdata.style.color="green";
+                                            outputdata.innerHTML=1;
+                                            setTimeout(()=>{
+                                                console.log("Happy Independence Day");
+                                                outputdata.style.color="rgb(223, 33, 65)";
+                                                outputdata.style.fontStyle="italic"
+                                                outputdata.innerHTML="Happy Independence Day";
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
+
+
+
+
